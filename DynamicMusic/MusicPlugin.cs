8d4ad88a9ee3c9d93fb0dffb293c0c861000fb7c
vibeside <vibeside01@gmail.com>
2024-02-02 13:32:17 -0500

    Added new class, updated controller, made auto populating music list

--- DynamicMusic/MusicPlugin.cs
+++ DynamicMusic/MusicPlugin.cs
@@ -1,9 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using DynamicMusic.Components;
+using UnityEngine;
 
 namespace DynamicMusic
 {
-    public class MusicPlugin
+    public enum MusicType
     {
+        Encounter,
+        Attack,
+        Escape
+    }
+    [BepInPlugin(modGUID,modName,"1.0.0.0")]
+    public class MusicPlugin : BaseUnityPlugin
+    {
+        public const string modGUID = "grugjosh.lethalcompany.DynamicMusic";
+        public const string modName = "DynamicMusic";
+        public static AssetBundle? bundle;
+        public static GameObject? musicController;
+        public static ManualLogSource mls;
+        public void Awake()
+        {
+            mls = base.Logger;
+            string sAssemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
+            bundle = AssetBundle.LoadFromFile(Path.Combine(sAssemblyLocation, "dynamicmusicbundle"));
+            musicController = new GameObject(modName);
+            musicController.AddComponent<MusicController>();
+            musicController.AddComponent<AudioSource>();
+            //On.GameNetcodeStuff.PlayerControllerB.Update += PlayerControllerB_Update;
+        }
+
+        //private void PlayerControllerB_Update(On.GameNetcodeStuff.PlayerControllerB.orig_Update orig, GameNetcodeStuff.PlayerControllerB self)
+        //{
+        //    self.sprintMeter = 1f;
+        //    orig(self);
+        //}
     }
 }
